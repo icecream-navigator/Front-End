72e95e94d28d273136541c0d81095fb08b4c7bbc
Robert Klinger <79660078+Eater0@users.noreply.github.com>
2021-11-15 01:03:55 +0100

    adding opening time

---
+++ src/components/TimeFrom.vue
@@ -0,0 +1,23 @@
+<template>
+  <Time @time="sendTime">
+    <template v-slot:content>
+      Od:
+    </template>
+  </Time>
+</template>
+
+<script>
+import Time from './Time.vue'
+
+export default {
+  name: 'TimeFrom',
+  components: {
+      Time
+  },
+  methods: {
+    sendTime(time) {
+      this.$emit('time', time)
+    }
+  }
+}
+</script>
